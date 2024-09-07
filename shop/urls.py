7032b8931df505d28c02ada3f347f5d1dd6637f3
Elwa88 <fsociety.core@gmail.com>
2024-09-07 16:59:34 +0400

    added views for managing cart and final order - adjusted urls.py accordingly

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import GetProduct, ListCarts, ListProducts
+from .views import GetProduct, MyCart, ListProducts, remove_from_cart, order
 
 urlpatterns = [
     path('', ListProducts.as_view()),
     path('add/<int:pk>/', GetProduct.as_view()),
-    path('my_cart', ListCarts.as_view())
+    path('my_cart/<int:owner_id>/', MyCart.as_view()),
+    path('my_cart/<int:owner_id>/remove/<int:id>/', remove_from_cart),
+    path('order/', order)
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import *
 from .serializers import *
 from rest_framework import permissions, response
-
+from django.shortcuts import HttpResponse
 
 class ListProducts(generics.ListAPIView):
     queryset = Product.objects.all()
@@ -34,7 +34,25 @@ class GetProduct(generics.RetrieveAPIView):
             'product': ProductSerializer(product).data
         })
 
-class ListCarts(generics.ListAPIView):
+class MyCart(generics.RetrieveUpdateAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    lookup_field = 'owner_id'
     permission_classes = [permissions.IsAuthenticated]
+
+def remove_from_cart(request,owner_id,id):
+    cart = Cart.objects.get(owner_id=owner_id)
+    products = cart.product.all()
+    product_name = products[id].name
+    cart.product.remove(products[id])
+    return HttpResponse(f'{product_name} has been removed from cart')
+
+
+def order(request):
+    cart = Cart.objects.get(owner = request.user)
+    products = cart.product.all()
+    total = 0
+    for product in products:
+        total += product.price
+        cart.product.remove(product)
+    return HttpResponse(f'Congratulations on your purchase. The total payment is {total}$. Your cart has been cleared')
